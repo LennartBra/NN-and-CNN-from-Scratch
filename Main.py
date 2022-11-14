525149ccad6e313a1c342fdadbe10622e5630473
Lennart Brakelmann <lennart.brakelmann005@stud.fh-dortmund.de>
2022-11-14 14:20:01 +0100

    First File

---
+++ Main.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Programmierprojekt - Coding a neural network from Scratch
+Autor: Lennart Brakelmann
+"""
+
+#%% Import packages and modules
+import numpy as np
+import matplotlib as plt
+#import layers as
+#import neurons as 
+#import 
+
+#%% Create Neural Network
